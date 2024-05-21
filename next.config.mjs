1f6a47d97a4012527805096ee498005ebf70c73f
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-05-21 20:07:53 +0530

    add github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
