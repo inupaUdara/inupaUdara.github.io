d579811818ad3217494f7662fe717eac5ff2ff76
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-05-21 21:54:46 +0530

    js to mjs

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export'
+};
+
+export default nextConfig;
