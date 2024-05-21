feb71eeeff8bba9b332f21836477eed04797de1f
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-05-21 20:51:50 +0530

    update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true,
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
