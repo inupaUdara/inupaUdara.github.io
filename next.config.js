31f40fa942bc6f28de19a7a079981e77e484707c
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-05-21 21:12:36 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = '';
-let basePath = '';
+let assetPrefix = "";
+let basePath = "";
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
 }
 
 module.exports = {
+    output: 'export',
   assetPrefix: assetPrefix,
   basePath: basePath,
   images: {
